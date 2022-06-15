8ccf347c19d290df3d7b9f0826013f421735eea0
Souma <553469159@qq.com>
2022-06-15 20:33:58 +0800

    don't use 'onPartyWipe'

--- ui/stream/stream.js
+++ ui/stream/stream.js
@@ -146,6 +146,8 @@ addOverlayListener("LogLine", (e) => {
       } else if (e.line[4] === "wipe") {
         reStartRecording();
       }
+    } else if(e.line[0] === "33" && /^4000001[026]$/.test(e.line[3])) {
+      partyWipe();
     }
 });
 
@@ -196,14 +198,16 @@ addOverlayListener('OnlineStatusChanged', (e) => {
   });
 });
 
-addOverlayListener('onPartyWipe', (e) => {
-  if (!isObsConnected) return;
-  if (!$('#wipe-check').is(':checked')) return;
-  // console.log("wipe");
-  reStartRecording();
-});
+// addOverlayListener('onPartyWipe', (e) => {});
 
 addOverlayListener('onGameExistsEvent', (e) => {
   if (!isObsConnected) return;
   stopRecording();
 });
+
+function partyWipe(){
+  if (!isObsConnected) return;
+  if (!$('#wipe-check').is(':checked')) return;
+  // console.log("wipe");
+  reStartRecording();
+}
